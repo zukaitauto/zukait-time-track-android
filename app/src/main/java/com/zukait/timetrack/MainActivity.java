f8ae3dec5ab45054545ce1bb35c24a5b95d9fa72
zukaitauto <zukaitauto@gmail.com>
2026-09-22 17:11:13 +0400

    Load V76 web assets

--- app/src/main/java/com/zukait/timetrack/MainActivity.java
+++ app/src/main/java/com/zukait/timetrack/MainActivity.java
@@ -211,7 +211,7 @@ public class MainActivity extends Activity {
         });
 
         webView.clearCache(true);
-        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=75");
+        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=76");
     }
 
     private String installedVersionName() {
