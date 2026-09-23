a7629f8e6b93753b46166ce638f478bc66474b2a
zukaitauto <zukaitauto@gmail.com>
2026-09-23 22:53:14 +0400

    Prepare V106 update: app/src/main/java/com/zukait/timetrack/MainActivity.java

--- app/src/main/java/com/zukait/timetrack/MainActivity.java
+++ app/src/main/java/com/zukait/timetrack/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends Activity {
         });
 
         webView.clearCache(true);
-        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=105");
+        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=106");
         handleUpdateInstallResult(getIntent());
         updateHandler.postDelayed(this::resumeUpdateDownloadMonitoring, 1200);
     }
