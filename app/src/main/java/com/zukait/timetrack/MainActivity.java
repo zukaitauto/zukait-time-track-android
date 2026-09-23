d47b835dc3e3611dcc076fe8114190bf2e9e780a
zukaitauto <zukaitauto@gmail.com>
2026-09-24 00:20:25 +0400

    Load V107 production assets

--- app/src/main/java/com/zukait/timetrack/MainActivity.java
+++ app/src/main/java/com/zukait/timetrack/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends Activity {
         });
 
         webView.clearCache(true);
-        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=106");
+        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=107");
         handleUpdateInstallResult(getIntent());
         updateHandler.postDelayed(this::resumeUpdateDownloadMonitoring, 1200);
     }
