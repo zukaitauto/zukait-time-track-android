27bd78639156185f6cc7c2d0d98a579aa2f241ba
zukaitauto <zukaitauto@gmail.com>
2026-09-24 02:26:50 +0400

    Update V112 app asset revision

--- app/src/main/java/com/zukait/timetrack/MainActivity.java
+++ app/src/main/java/com/zukait/timetrack/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends Activity {
         });
 
         webView.clearCache(true);
-        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=111");
+        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=112");
         handleUpdateInstallResult(getIntent());
         updateHandler.postDelayed(this::resumeUpdateDownloadMonitoring, 1200);
     }
