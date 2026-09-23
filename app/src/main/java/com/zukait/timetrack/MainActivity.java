34f8bb8715aa9a7525614c473421de71e0818307
zukaitauto <zukaitauto@gmail.com>
2026-09-24 01:03:21 +0400

    Update V108 asset cache version

--- app/src/main/java/com/zukait/timetrack/MainActivity.java
+++ app/src/main/java/com/zukait/timetrack/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends Activity {
         });
 
         webView.clearCache(true);
-        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=107");
+        webView.loadUrl("https://" + APP_HOST + "/assets/offline_test.html?v=108");
         handleUpdateInstallResult(getIntent());
         updateHandler.postDelayed(this::resumeUpdateDownloadMonitoring, 1200);
     }
